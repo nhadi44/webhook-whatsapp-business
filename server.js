04a0fb3268219c378b616654ebf3ec690097d4c4
nhadi44 <hadinurhidayat97@hotmail.com>
2024-09-08 15:59:55 +0700

    add config webhook whatsapp

--- server.js
+++ server.js
@@ -8,6 +8,41 @@ app.get('/', (req, res) => {
    res.send('Hello World!');
 });
 
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+   try {
+      const body = req.body; // Mengambil body dari request
+      console.log('Received POST request:', body);
+
+      if (body.messages) {
+         body.messages.forEach((message) => {
+            console.log(`Message ID: ${message.id}`);
+            console.log(`Status: ${message.status}`);
+            console.log(`Timestamp: ${new Date(message.timestamp * 1000).toISOString()}`);
+         });
+      }
+
+      res.json({ message: 'Received POST request', data: body });
+   } catch (error) {
+      console.error('Error parsing request body:', error);
+      res.status(400).json({ message: 'Failed to parse request body' });
+   }
+});
+
+app.get('/webhook', (req, res) => {
+   const VERIFY_TOKEN = 'secret';
+   const { mode, token, challenge } = req.query;
+
+   if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('Webhook verified successfully');
+      res.status(200).send(challenge);
+   } else {
+      console.error('Failed to verify webhook');
+      res.status(403).json({ message: 'Failed to verify webhook' });
+   }
+});
+
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);
 });
